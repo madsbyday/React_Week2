2eb52d7008e3e75b822384811c591ed608042e03
Mads Andersen <cph-ma370@cphbusiness.dk>
2017-10-26 15:39:36 +0200

    Day 2 EX Seed
    !!!!No target or node-modules folders

---
+++ React_seed/seedClient-master/src/pages/App.js
@@ -0,0 +1,27 @@
+import React from "react"
+import {Route, Switch } from "react-router-dom"
+import Login from "./Login";
+import Logout from "./Logout";
+import About from "./About";
+import UserPage from "./UserPage";
+import AdminPage from "./AdminPage";
+import TopMenu from "./TopMenu";
+import NumberPage from "./NumberPage";
+
+
+function App() {
+  return (
+    <div>
+      <TopMenu />
+      <Switch>
+        <Route path="/login" component={Login} />
+        <Route path="/logout" component={Logout} />
+        <Route path="/about" component={About} />
+        <Route path="/user" component={UserPage} />
+        <Route path="/admin" component={AdminPage} />
+        <Route path="/number" component={NumberPage} />
+      </Switch>
+    </div>
+  )
+}
+export default App;
